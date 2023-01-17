a28cbe6d4fef6977428d88ee65a2eec8fcbb0b81
Georgi Dimitrov Dimitrov <60718032+GreoN5@users.noreply.github.com>
2023-01-18 00:53:37 +0200

    fix intents

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from discord.ext import commands
 from music import Music
 from helpers import Help
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 bot.remove_command('help')
 
 client = discord.Client(intents=discord.Intents.default())
