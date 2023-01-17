7aad51de049a46846116f9dbd518bf35c1abaa64
Georgi Dimitrov Dimitrov <60718032+GreoN5@users.noreply.github.com>
2023-01-18 00:40:29 +0200

    fix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 from music import Music
 from helpers import Help
 
-bot = commands.Bot('!', intents=discord.Intents.default())
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.default())
 bot.remove_command('help')
 
 client = discord.Client(intents=discord.Intents.default())
