6af655421875fcda291d4a05bba7edde3caa8a6f
Georgi Dimitrov Dimitrov <60718032+GreoN5@users.noreply.github.com>
2023-09-30 16:05:34 +0300

    feat: add pause/resume commands

--- commands/pause.js
+++ commands/pause.js
@@ -2,20 +2,20 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('skip').setDescription('Skips the current song'),
+  data: new SlashCommandBuilder().setName('pause').setDescription('Pauses the current song'),
   async execute(interaction) {
     const queue = interaction.client.player.queues.get(interaction.guildId);
 
     if (!queue) {
-      await interaction.reply('There are no songs in the queue.');
+      await interaction.reply('There is no song playing.');
       return;
     }
 
     const currentSong = queue.currentTrack;
-    queue.node.skip();
+    queue.node.setPaused(true);
 
     const embed = new EmbedBuilder()
-      .setDescription(`${currentSong.title} has been skipped!`)
+      .setDescription(`${currentSong.title} has been paused!`)
       .setThumbnail(currentSong.thumbnail);
 
     await interaction.reply({
